8e769eaaf992e556d1876f84962a2fd3f37c5471
pipizheWang <batqzdepipi@outlook.com>
2025-11-17 21:37:57 +0800

    修正油门映射逻辑

--- nl_controller/traj_controller_NF.py
+++ nl_controller/traj_controller_NF.py
@@ -222,7 +222,8 @@ class TrajController(Node):
         attitude_target = AttitudeTarget()
 
         # 计算归一化推力值（考虑效率）
-        normalized_thrust = np.sqrt(thrust / self.gravity * self.thrust_efficiency * self.thrust_efficiency)
+        # normalized_thrust = np.sqrt(thrust / self.gravity * self.thrust_efficiency * self.thrust_efficiency)
+        normalized_thrust = -0.0015 * thrust * thrust + 0.0764 * thrust + 0.1237
         attitude_target.thrust = np.clip(normalized_thrust, 0.0, 1.0)
 
         actual_thrust_vector = body_z * thrust
